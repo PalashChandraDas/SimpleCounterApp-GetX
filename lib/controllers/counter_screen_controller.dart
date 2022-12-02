b8793c80f0be3ab3d628b325a4ebb93b05d186f9
Palash Chandra Das <palashdimik@gmail.com>
2022-12-02 20:37:35 +0600

    added onInit_Method

--- lib/controllers/counter_screen_controller.dart
+++ lib/controllers/counter_screen_controller.dart
@@ -4,11 +4,20 @@ import 'package:get/get.dart';
 class CounterScreenController extends GetxController{
   var myCounter = 0.obs;
 
+  @override
+  void onInit() {
+    increment();
+    decrement();
+    myReset();
+
+    super.onInit();
+  }
+
   void increment(){
     myCounter.value++;
   }
   void decrement(){
-    
+
     myCounter > 0 ? myCounter.value-- : Fluttertoast.showToast(msg: 'Already Zero');
   }
   void myReset(){
